a1abe95a63134e49839945b9c1e84105cd7a03e8
penghuidong <penghuidong520@gmail.com>
2023-04-03 21:46:04 -0400

    update bird

--- Assets/Bird.cs
+++ Assets/Bird.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Ridgidbody2D myBird;
+    public Ridgidbody2D myRidgidbody;
     void Start()
     {
         
