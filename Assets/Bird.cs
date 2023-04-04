09f507c8a1c113e1ae35591a15231e2b9183f4cc
penghuidong <penghuidong520@gmail.com>
2023-04-03 21:34:05 -0400

    update bird

---
+++ Assets/Bird.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Ridgidbody2D myBird;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
